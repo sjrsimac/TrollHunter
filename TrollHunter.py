ddbfbcc917a8345044f0a853e275dd95797e9dd6
sjrsimac <sjrsimac@gmail.com>
2017-04-28 01:47:03 -0400

    Update TrollHunter.py

--- TrollHunter.py
+++ TrollHunter.py
@@ -5,6 +5,12 @@ import nltk
 import re
 import csv
 
+# If I can process one post in 3.7 seconds, how often do we get a new post?
+# There are 37 posts per hour, which means it takes 98 seconds to make a post.
+# Holy shit, we make a post every 98 seconds!
+
+# I have to trun 
+
 #I'm just being safe and keeping my credentials in a separate file.
 reddit = praw.Reddit(client_id = client_id,
                      client_secret = client_secret,
@@ -69,25 +75,26 @@ def ProcessPost(submission): # Creates the flags for a single post.
 
 #This is where I initialize the dictionary that will hold all of the information, and pull older data from the csv file on my computer.
 Rows = {}
-##with open('posts.csv') as posts:
-##    reader = csv.DictReader(posts)
-##    for i in reader:
-##        Rows[i['LinkID']] = [i['Troll'],i['AuthorKarmaAgeSpread'],i['LexicalDiversity'],i['FormattingCount'],i['KarmaPerPost'],i['OPSelfCommentRate']]
+with open('posts.csv') as posts:
+    reader = csv.DictReader(posts)
+    for i in reader:
+        Rows[i['LinkID']] = [i['Troll'],i['AuthorKarmaAgeSpread'],i['LexicalDiversity'],i['FormattingCount'],i['KarmaPerPost'],i['OPSelfCommentRate']]
 
 # This is where the variables are created.
-for i, submission in enumerate(reddit.subreddit(OurSubreddit).hot(limit=100)):
+for i, submission in enumerate(reddit.subreddit(OurSubreddit).hot(limit=30)):
     Rows[ProcessPost(submission)[0]] = ProcessPost(submission)[1:7]
 
 # Moderators flag trolls by selecting the 'Trolling' reason in the modqueue. The bot will remove it and then note the post as a troll.
 for item in reddit.subreddit(OurSubreddit).mod.reports(limit=100):
-    if item.mod_reports[0][0] == 'Trolling':
-        if type(item) == praw.models.reddit.comment.Comment:
-##            item.mod.remove() # Do not uncomment this line until we go live!
-            print("This is a comment.")
-        elif type(item) == praw.models.reddit.submission.Submission:
-            print("This is a submission.", item.id)
-            Rows[item.id][0] = 1
-##            item.mod.remove() # Do not uncomment this line until we go live!
+    if len(item.mod_reports) == 1:
+        if item.mod_reports[0][0] == 'Trolling':
+            if type(item) == praw.models.reddit.comment.Comment:
+                item.mod.remove() # Do not uncomment this line until we go live!
+##                print("This is a comment.")
+            elif type(item) == praw.models.reddit.submission.Submission:
+##                print("This is a submission.", item.id)
+                Rows[item.id][0] = 1
+                item.mod.remove() # Do not uncomment this line until we go live!
 
 # This is the machine learning portion of the program. It's commented out because I want to collect data before making a model.
 ##from sklearn import tree # Visit http://scikit-learn.org/stable/install.html to learn how to install this package.
